56e15f457c125fd794b4e4599b73d40e40189e6b
zhangyunduan <zhangyunduan@linyang.com.cn>
2025-09-22 03:17:15 +0000

    Enable fastboot and adjust to USB1

--- drivers/usb/gadget/rcar/renesas_usb.h
+++ drivers/usb/gadget/rcar/renesas_usb.h
@@ -440,7 +440,7 @@ static inline void *phy_get_drvdata(struct phy *phy)
 
 
 #if defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G043U) || defined(CONFIG_R9A07G054L) || defined(CONFIG_ARCH_RZMPU)
-#define PHY_BASE	0x11c50200
+#define PHY_BASE	0x11c70200
 #define RCAR3_PHY_DEVICE "RZG2L-PHY "
 #define USBHS_BASE	0x11c60000
 #define RCAR3_USBHS_DEVICE "RZG2L-USBHS "
