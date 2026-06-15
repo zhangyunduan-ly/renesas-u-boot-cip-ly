f8f88c9e40eeaa14be3a3c4eccbcabbfd0296f21
zhangyunduan <zhangyunduan@linyang.com.cn>
2026-06-15 10:40:53 +0800

    board: rzg2ul-dev: Modify IO allocation

--- board/renesas/rzg2ul-dev/rzg2ul-dev.c
+++ board/renesas/rzg2ul-dev/rzg2ul-dev.c
@@ -165,13 +165,13 @@ enum Rzg2ULGpioPins_E {
 };
 
 /* PERIPHERAL Power Control */
-#define PERIPHERAL_POWER_GPIO	RZG2UL_P07_1
+#define PERIPHERAL_POWER_GPIO	RZG2UL_P10_3
 
 /* LED */
-#define RUN_LED_GPIO			RZG2UL_P18_0
+#define RUN_LED_GPIO			RZG2UL_P16_0
 
 /* WDT */
-#define WDT_GPIO				RZG2UL_P12_1
+#define WDT_GPIO				RZG2UL_P9_3
 
 void s_init(void)
 {
